14e9dc445d8517510bda068f58258ccd278029ac
William Baele <baelewilliam1999@gmail.com>
2023-10-02 15:55:58 +0200

    ReadMe

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-## WiP
+## Gymeal
 
-gymeal.xyz
+Gymeal helps people how are lost when they need to change a bit the meals they eat. Just ask to Gymeal and get some tasty recipes !
+
+You can set a filter on the protein source you are craving for and ask a specific meals number
+
+[Gymeal](https://www.gymeal.xyz)
--- frontend/src/components/WelcomeClient.js
+++ frontend/src/components/WelcomeClient.js
@@ -4,7 +4,7 @@ const WelcomeClient = ({ openForm }) => {
   return (
     <div className="flex flex-col items-center justify-center w-full h-full space-y-6">
       <h1 className="text-4xl text-center md:text-5xl">
-        Stop eating always the same boring meals
+        Stop always eating the same boring meals
       </h1>
       <p className="text-lg text-center md:text-xl">
         Gymeal help you to variate your protein source and keep enjoying eating
